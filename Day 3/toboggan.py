55624a6fa6bfaf507b3baad353caeaa901f4e830
Martin Törnqvist <gris_martin@hotmail.com>
2020-12-03 18:55:28 +0100

    Remove debug printouts

--- Day 3/toboggan.py
+++ Day 3/toboggan.py
@@ -10,13 +10,10 @@ y_pos = down
 
 with open(sys.argv[1], 'r') as f:
     rows = f.readlines()
-    print(rows)
     width = len(rows[0]) - 1  # Don't count last newline character
     y_end = len(rows)
     while y_pos < y_end:
         row = rows[y_pos]
-        print(f"Hit {row[x_pos % width]} on row {y_pos}, column {x_pos} ({x_pos % width}). Width: {width}")
-        print(f"row is {row}")
         if row[x_pos % width] == '#':
             trees += 1
         y_pos += down
